6dedbb4097f666a25b09f57e486de5f690bc2fd5
William Lowrimore <92553989+wlowrimore@users.noreply.github.com>
2022-02-28 13:58:05 -0600

    adds call to database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +7,21 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: '@Vandy#2022_bc/19!',
+    database: 'election'
+  },
+  console.log('Connected to the election database.')
+);
+
+db.query('SELECT * FROM candidates', (err, rows) => {
+  console.log(rows);
+});
 
 
 // Default response for any other request (Not Found)
